4d09ba02439e305270b6ba746f3adaf47980f99b
Frodo <nosoysaenz@gmail.com>
2020-03-15 23:13:54 -0600

    test 3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(){
     return (
         <footer className='Footer'>
-            <h6>This is a footer</h6>
+            <h6>This is a footer!</h6>
         </footer>
     )
 }
